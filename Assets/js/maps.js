2d91d6802113ca957d2267beb78c00fdb71cb1f4
juanjchavez <juan.chavezn1990@gmail.com>
2021-02-03 11:37:15 -0600

    adding Vivus and removing jquery

--- Assets/js/maps.js
+++ Assets/js/maps.js
@@ -1,9 +1,13 @@
-$(document).ready(function(){
-    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+//VIVUS Initialization
+new Vivus('Ni', 
+            {duration: 120, type:'oneByOne'}
+      );
+//Enable popover component
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
     var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
       return new bootstrap.Popover(popoverTriggerEl)
     })
-    var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
-        trigger: 'focus'
-      })
-});
+//Dismiss popover on lost focus
+var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+    trigger: 'focus'
+  })
